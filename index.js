da2bbed99f55ffdb7ef47b5c9b79ac36fa7b2018
Annette Poh <coding.net.p@gmail.com>
2021-11-17 22:33:12 +0800

    initial setup for admin site

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+require("dotenv").config();
+
+// create an instance of express app
+let app = express();
+
+// set the view engine
+app.set("view engine", "hbs");
+
+// static folder
+app.use(express.static("public"));
+
+// setup wax-on
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
+
+// enable forms
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+// import in routes
+const homeRoutes = require('./routes/home');
+
+async function main() {
+
+    app.use('/', homeRoutes);
+  
+}
+
+main();
+
+app.listen(3000, () => {
+  console.log("Server has started");
+});
---
+++ routes/home.js
@@ -0,0 +1,10 @@
+const express = require("express");
+// create a new router object, Router() will return a new router object
+const router = express.Router(); 
+
+// A router object can contain routes - Add a new route to the Express router
+router.get('/', (req,res)=>{
+    res.render("home/index")
+})
+
+module.exports = router; 
