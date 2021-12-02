e1e46036dd296d77385c36d735adc76438d2b644
Annette Poh <coding.net.p@gmail.com>
2021-12-02 02:05:08 +0000

    add initial logic to retrieve carts from backend api

---
+++ dal/carts.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+// get all carts
+const getAllCarts = async (refreshToken) => {
+
+    let accessTokenResult = await axios.post(
+        `${apiUrl}/users/refresh`, 
+        {
+            "refresh_token": refreshToken
+        }, 
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    let accessToken = accessTokenResult.data.accessToken
+
+    let cartsResult = await axios.get(
+        `${apiUrl}/carts/all`, 
+        { 'headers': {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+    let allCarts = cartsResult.data.data;
+    return allCarts;
+    
+}
+
+module.exports = {
+    getAllCarts
+}
--- routes/carts.js
+++ routes/carts.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('carts/listing', {})
+const cartDataLayer = require("../dal/carts");
+
+router.get('/', async (req, res) => {
+    let carts = await cartDataLayer.getAllCarts(req.session.user.token);
+    carts.forEach(cart => {
+        cart.numberOfItems = cart.products.length
+        let total = 0
+        cart.products.forEach( product => {
+            total += parseFloat(product.selling_price) * product["_pivot_quantity"]
+        })
+        cart.totalAmount = total.toFixed(2)
+    })
+    res.render('carts/listing', {
+        carts: carts
+    })
 })
 
 // route to process tp update cart orders that was sent abandoned email
